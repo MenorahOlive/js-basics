7a15492b27bc3d3fa1651c6f33890d798727b9b9
Olive <olivemenorah@gmail.com>
2024-05-29 21:24:41 +0300

    Dynamic Programming

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-//promitive types 
+//primitive types 
 let name = 'Olive'; //String Literal 
 let age = 30; //Number Literal
 let isApproved = false; //Boolean Literals
 let firstName = undefined;
 let selectedColor = null;
+
+//JS is an Dynamic Programming language
+//Change the datatype by reassigning 
+//Check the value using type of variableName
+
