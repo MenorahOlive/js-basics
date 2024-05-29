d7d372b730b5d13fdad7322d30b1633b2d247135
Olive <olivemenorah@gmail.com>
2024-05-29 21:08:58 +0300

    Variables

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-//this is my first script
-console.log('Hello World');
+//declaring variables
+let name='Olive';
+console.log(name);
+
+//Rules for variables
+//Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number 
+//cannot contain a space or hyphen 
+//are case sensitive
+//naming conventions are camelCase
+
+let firstName='Olive';
+let lastName='Menorah';
+
+
