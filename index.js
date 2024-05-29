5d0fc3ce7c14fe340495e4517a8558f7b0bd96a2
Olive <olivemenorah@gmail.com>
2024-05-29 21:12:18 +0300

    Constants

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-//declaring variables
-let name='Olive';
-console.log(name);
-
-//Rules for variables
-//Cannot be a reserved keyword
-//Should be meaningful
-//Cannot start with a number 
-//cannot contain a space or hyphen 
-//are case sensitive
-//naming conventions are camelCase
-
-let firstName='Olive';
-let lastName='Menorah';
-
-
+const interestRate = 1;
+console.log(interestRate);
