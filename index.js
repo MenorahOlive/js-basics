bd6a14a8ceddc8ce02e8ece978d183114f33c840
Olive <olivemenorah@gmail.com>
2024-05-29 21:33:22 +0300

    Objects

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-//primitive types 
-let name = 'Olive'; //String Literal 
-let age = 30; //Number Literal
-let isApproved = false; //Boolean Literals
-let firstName = undefined;
-let selectedColor = null;
+//Reference types- arrays, objects and functions
 
-//JS is an Dynamic Programming language
-//Change the datatype by reassigning 
-//Check the value using type of variableName
+//objects 
+let person = {
+    name: 'Olive',
+    age: 22
+};
 
+//accessing an oject 
+// dot notation 
+person.name = 'Livvy';
+
+//bracket notation 
+let selection = name;
+person['selection'] = 'Liv';
+
+console.log(person.name);
